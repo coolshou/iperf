fec416e268be306cf87fb91b01105b1215cbb20e
coolshou <jimmy_yeh@alphanetworks.com>
2025-03-25 19:48:53 +0800

    Update project news for iperf-3.14.
    Doesn't quite build due to some platform issues.

--- docs/conf.py
+++ docs/conf.py
@@ -132,6 +132,7 @@ html_logo = "_static/esnet/ESnet_Final_Logos_All_Blue_Circle_Stamp_RGB.png"
 
 
 
+
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
