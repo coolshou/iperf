06649352b6d5d8f9f87e97ed2d07bdffd5f60e9a
coolshou <jimmy_yeh@alphanetworks.com>
2025-03-25 18:49:25 +0800

    Fix #1534 - prevent udp packet count and operations overflow

--- src/iperf_udp.c
+++ src/iperf_udp.c
@@ -353,7 +353,7 @@ iperf_udp_buffercheck(struct iperf_test *test, int s)
     if (sndbuf_actual_item == NULL) {
 	cJSON_AddNumberToObject(test->json_start, "sndbuf_actual", sndbuf_actual);
     }
-        
+
     cJSON *rcvbuf_actual_item = cJSON_GetObjectItem(test->json_start, "rcvbuf_actual");
     if (rcvbuf_actual_item == NULL) {
 	cJSON_AddNumberToObject(test->json_start, "rcvbuf_actual", rcvbuf_actual);
